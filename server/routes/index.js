a6dded8fd442ef603b1158742c6ddfea14a09991
Daniel Abrão <dcerag@br.ibm.com>
2017-05-06 19:18:52 -0300

    fixing travis

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@
     module.exports = function (app, dependencies) {
 
         app.get("/", function (req, res) {
-            return res.status(200).send("oi");
+            return res.status(200).send("ois");
         });
     };
 
