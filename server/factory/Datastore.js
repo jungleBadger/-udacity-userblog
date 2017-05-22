f2eea292a33b879fcd647010bae638413de9b3c2
Daniel Abrão <dcerag@br.ibm.com>
2017-05-22 11:54:33 -0300

    changes test

--- server/factory/Datastore.js
+++ server/factory/Datastore.js
@@ -12,17 +12,52 @@
         });
 
         return {
-            "saveData": function (intent, data) {
+            "saveData": function (entity, data) {
                 return new Promise(function (resolve, reject) {
-                    var key = datastoreClient.key(intent);
-                    datastoreClient.save({
-                        "key": key,
-                        "data": data
-                    }).then(function (response) {
-                        resolve(response);
-                    }).catch(function (err) {
+                    if (entity && typeof data === "object") {
+                        var key = datastoreClient.key(entity);
+                        datastoreClient.save({
+                            "key": key,
+                            "data": data
+                        }).then(function (response) {
+                            resolve(response);
+                        }).catch(function (err) {
+                            reject(err);
+                        });
+                    } else {
+                        throw new Error("Can not proceed without entity and payload");
+                    }
+
+                });
+            },
+            "listData": function (entity, key) {
+                return new Promise(function (resolve, reject) {
+                    if (entity && key) {
+                        var query = datastoreClient.createQuery(entity);
+                        query.filter(key, ">", null);
+                        query.order(key, {
+                            "descending": true
+                        });
+                        datastoreClient.runQuery(query).then(function (items) {
+                            resolve(items);
+                        }).catch(function (err) {
+                            reject(err);
+                        });
+                    } else {
+                        throw new Error("Can not proceed without entity and search key");
+                    }
+                });
+            },
+            "queryData": function (entity, queryFilters) {
+                return new Promise(function (resolve, reject) {
+                    if (entity && (queryFilters && queryFilters.length)) {
+                        var query = datastoreClient.createQuery(entity);
+                        resolve();
                         reject(err);
-                    });
+                    } else {
+                        throw new Error("Can not proceed without entity");
+                    }
+
                 });
             }
         };
